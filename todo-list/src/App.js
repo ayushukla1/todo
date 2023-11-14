b5b306d6742b3f95525638bc37e36484997b16c3
Ayush Shukla <82086744+ayushukla1@users.noreply.github.com>
2023-11-14 06:29:36 +0000

    updated Readme

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -2,22 +2,24 @@ import './App.css';
 import Header from './MyComponents/Header';
 import Todos from './MyComponents/Todos';
 import Footer from './MyComponents/Footer';
-import React, { useState, useEffect } from 'react';
 import AddTodo from './MyComponents/AddTodo';
+import About from './MyComponents/About';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   let initTodo;
-  if(localStorage.getItem("todos") === null){
-    initTodo=[];
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
   }
-  else{
+  else {
     initTodo = JSON.parse(localStorage.getItem("todos"));
   }
 
   const onDelete = (todo) => {
     console.log("I am ondelete of todo", todo);
-    setTodos(todos.filter((e)=>{
-      return e!==todo;
+    setTodos(todos.filter((e) => {
+      return e !== todo;
     }));
     console.log("deleted", todos);
     localStorage.setItem("todos", JSON.stringify(todos));
@@ -32,27 +34,37 @@ function App() {
     else {
       sno = todos[todos.length - 1].sno + 1;
     }
-    const myTodo={
-      sno:sno,
-      title:title,
-      desc:desc,
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
     }
     setTodos([...todos, myTodo]);
     console.log(myTodo);
   }
 
   const [todos, setTodos] = useState(initTodo);
-  
+
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos])
 
   return (
     <div>
-      <Header title="My Todos List"/>
-      <AddTodo addTodoList={addTodoList}/>
-      <Todos todos={todos} onDelete={onDelete}/>
-      <Footer/>
+      <Router>
+        <Header title="My Todos List" />
+        <Routes>
+          <Route exact path="/" element={
+            <>
+              <AddTodo addTodoList={addTodoList} />
+              <Todos todos={todos} onDelete={onDelete} />
+            </>
+          } />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
--- todo-list/src/MyComponents/About.js
+++ todo-list/src/MyComponents/About.js
@@ -1,13 +1,10 @@
-
-import React from 'react';
-
 function About() {
-  return (
-    <div>
-      <h2>About</h2>
-      <p>This is a simple todo list app built with React.</p>
-    </div>
-  );
-}
-
-export default About;
+    return (
+      <div>
+        <h2>About</h2>
+        <p>This is the About page of our Todo application. Our application allows you to manage your daily tasks effectively. You can add, delete and view your tasks. This application is built using React.</p>
+      </div>
+    );
+  }
+  
+  export default About;
