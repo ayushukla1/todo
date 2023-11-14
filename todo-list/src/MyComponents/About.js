b0c6c877868a5dde22a76299864a931507eec35e
Ayush Shukla <82086744+ayushukla1@users.noreply.github.com>
2023-11-14 06:21:42 +0000

    Added About page

---
+++ todo-list/src/MyComponents/About.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>This is a simple todo list app built with React.</p>
+    </div>
+  );
+}
+
+export default About;
